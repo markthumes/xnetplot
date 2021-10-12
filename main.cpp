cc8328627fbdb979a960856f5d730f8204fe0111
markthumes@gmail.com <markthumes>
2021-10-12 18:17:06 -0500

    Moved XEvent handling into graph class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "graph.h"
+#include <stdio.h>
 #include <unistd.h>
 
 int main(){
@@ -7,30 +8,34 @@ int main(){
 	g.open();
 	g.setBackground( X11::Color(0,0,0) );
 	g.setDoubleBuffered();
-	bool open = true;
-	Graph::Series s;
-	s.color = X11::Color(0,1,1);
-	s.width = 3;
-	s.modifier = Graph::Lines;
+
+
+	Graph::Series s[3];
+	s[0].color = X11::Color(0,1,1);
+	s[0].width = 3;
+	s[0].modifier = Graph::Lines;
 	for( int i = -100; i < 100; i++ ){
-		s.points.push_back( X11::Point( i, i*i/10 ) );
+		s[0].points.push_back( X11::Point( i, i*i/10 ) );
 	}
-	g.addSeries( s );
+	g.addSeries( s[0] );
+
+	s[1].color = X11::Color(1,1,0);
+	s[1].modifier = Graph::Points;
+	for( int i = 0; i < 100; i++ ){
+		s[1].points.push_back( X11::Point( i*4, i ) );
+	}
+	g.addSeries( s[1] );
+
+	s[2].color = X11::Color(1,0,1);
+	s[2].modifier = Graph::Lines;
+	for(int i = -100; i < 100; i++ ){
+		s[2].points.push_back( X11::Point( i*i, 10 + i ));
+	}
+	g.addSeries( s[2] );
+
 	g.setPadding( 100, 100 );
-	while(open){
-		while( XPending(g.getDisplay()) ){
-			XEvent xev;
-			XNextEvent( g.getDisplay(), &xev );
-			if( xev.type == Expose ){
-				g.getAttributes();
-				g.refresh();
-			}
-			if( xev.type == KeyPress ){
-				open = false;
-			}
-		}
-		g.render();
-		g.refresh(frate);
+	while(g.isOpen()){
+		g.update(frate);
 	}
 	g.close();
 	return 1;
