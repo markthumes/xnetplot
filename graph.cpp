67ecc445ca50f21d7abd159842ab61b5e3306547
markthumes@gmail.com <markthumes>
2021-10-13 12:38:57 -0500

    Added series passing by reference for "Dynamic Series"

--- graph.cpp
+++ graph.cpp
@@ -6,6 +6,8 @@ Graph::Graph( const char* name, int width, int height ){
 	m_attrib.width = width;
 	m_attrib.height = height;
 	m_padding = X11::Point(0,0);
+	m_dynamicSeriesCount = 0;
+	m_dynamicSeries = NULL;
 }
 void Graph::drawFrame(){
 	X11::Point topLeft ( m_padding.x, m_padding.y );
@@ -31,6 +33,25 @@ void Graph::render(){
 		else if( m_series[i].modifier == Points ){
 			drawPoints( m_series[i].color, inverted, count );
 		}
+		delete [] inverted;
+	}
+	for( unsigned int i = 0; i < m_dynamicSeriesCount; i++ ){
+		if( m_dynamicSeries == NULL ) break;
+		if( m_dynamicSeries[i] == NULL ) break;
+		if( m_dynamicSeries[i]->valid == false ) continue;
+		int count = m_dynamicSeries[i]->points.size();
+		X11::Point *inverted = new X11::Point[count];
+		for( int j = 0; j < count; j++ ){
+			inverted[j].x =                   m_padding.x + m_dynamicSeries[i]->points[j].x;
+			inverted[j].y = m_attrib.height - m_padding.y - m_dynamicSeries[i]->points[j].y;
+		}
+		if( m_series[i].modifier == Lines ){
+			drawLines( m_dynamicSeries[i]->color, (X11::Line*)inverted, count / 2 );
+		}
+		else if( m_series[i].modifier == Points ){
+			drawPoints( m_dynamicSeries[i]->color, inverted, count );
+		}
+		delete [] inverted;
 	}
 }
 
@@ -45,6 +66,11 @@ int Graph::addSeries( Series &s ){
 	}
 	m_series.push_back(s);
 	return s.ID = m_series.size() - 1;
+}
+int Graph::addSeries( Series* s ){
+	m_dynamicSeriesCount++;
+	m_dynamicSeries = (Graph::Series**)realloc( m_dynamicSeries, sizeof( Series* ) * m_dynamicSeriesCount );
+	m_dynamicSeries[m_dynamicSeriesCount-1] = s;
 }
 
 void Graph::removeSeries( Series& s ){
--- graph.h
+++ graph.h
@@ -1,5 +1,6 @@
 #include "x11wrapper/x11wrapper.h"
 #include <vector>
+#include <stdlib.h>
 #include <unistd.h>
 
 class Graph : public X11 {
@@ -13,6 +14,7 @@ public:
 		Series(){
 			valid = true;
 			ID = -1;
+			modifier = Points;
 		}
 		int ID;
 		bool valid;
@@ -24,6 +26,7 @@ public:
 public:
 	Graph( const char* name, int width, int height );
 	int addSeries( Series &s );
+	int addSeries( Series *s );
 	void removeSeries( Series &s );
 	void setPadding( int x, int y ){ m_padding = X11::Point( x, y ); }
 
@@ -32,7 +35,9 @@ protected:
 	void drawFrame();
 	void render();
 private:
-	std::vector<Series> m_series;
+	std::vector<Series>  m_series;
+	Series** m_dynamicSeries;
+	int m_dynamicSeriesCount;
 	X11::Point m_padding;
 	X11::Point m_origin;
 };
